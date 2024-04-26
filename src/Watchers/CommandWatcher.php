1145217f0a6aa4a7b89dadbe85ef5d0c29d29068
overtrue <anzhengchao@gmail.com>
2024-04-26 17:51:33 +0800

    wip: cli

---
+++ src/Watchers/CommandWatcher.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Overtrue\LaravelOpenTelemetry\Watchers;
+
+use Illuminate\Console\Events\CommandFinished;
+use Illuminate\Console\Events\CommandStarting;
+use Illuminate\Contracts\Foundation\Application;
+use OpenTelemetry\API\Trace\SpanInterface;
+use OpenTelemetry\Context\Context;
+use Overtrue\LaravelOpenTelemetry\Facades\Measure;
+
+class CommandWatcher implements Watcher
+{
+    protected SpanInterface $span;
+
+    public function register(Application $app): void
+    {
+        $app['events']->listen(CommandStarting::class, function (CommandStarting $event) {
+            $this->span = Measure::span('[Command] '.$event->command)
+                ->setAttributes([
+                    'command' => $event->command,
+                    'arguments' => $event->input->getArguments(),
+                    'options' => $event->input->getOptions(),
+                ])
+                ->start();
+            $this->span->storeInContext(Context::getCurrent());
+        });
+
+        $app['events']->listen(CommandFinished::class, function (CommandFinished $event) {
+            $this->span->end();
+        });
+    }
+}
